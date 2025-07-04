ac1fd90cbf760441e66ccd97cf0db584e4ca2587
Darren Knipe <darren@darkolive.co.uk>
2025-07-04 23:23:55 +0100

    feat: initialize project with auth, course, and TTS agent infrastructure

--- modus/go.mod
+++ modus/go.mod
@@ -1,5 +1,7 @@
 module modus
 
-go 1.23.5
+go 1.23.1
+
+toolchain go1.23.5
 
 require github.com/hypermodeinc/modus/sdk/go v0.18.0
--- modus/main.go
+++ modus/main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	_ "github.com/hypermodeinc/modus/sdk/go"
 )
 
-func SayHello(name *string) string {
-
-	var s string
-	if name == nil {
-		s = "World"
-	} else {
-		s = *name
-	}
-
-	return fmt.Sprintf("Hello, %s!", s)
+func main() {
+	// Explorer will use the exported functions
+	// No actual implementation needed here
 }
